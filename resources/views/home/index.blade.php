c2061cda9387e80a9b5a85a634096fbc8b9dc36e
NEGIBOSE <ryo0827ngsh@icloud.com>
2024-03-10 00:21:41 +0900

    home_htmlのみ

--- resources/views/home/index.blade.php
+++ resources/views/home/index.blade.php
@@ -6,7 +6,22 @@
     <title>HOME</title>
 </head>
 <body>
-    <h1>つぶやきアプリ</h1>
-    <p>{{ $name }}</p>
+    <div class="home">
+        <div class="home_upper">
+            <div class="home_icon"></div>
+            <div class="home_pic">
+                <img src="" alt="">
+            </div>
+        </div>
+        <div class="home_select">
+            <div class="home_select_u">
+                <a href="#">本を寄贈</a>
+                <a href="#">本棚</a>
+            </div>
+            <div class="home_select_l">
+                <a href="#">成長記録を見る</a>
+            </div>
+        </div>
+    </div>
 </body>
 </html>
