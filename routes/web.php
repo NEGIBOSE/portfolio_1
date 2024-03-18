430705bdd3898f0f2724cbb3dc12d2e7033b17ba
NEGIBOSE <ryo0827ngsh@icloud.com>
2024-03-18 15:08:19 +0900

    /home, /search連動

--- routes/web.php
+++ routes/web.php
@@ -17,12 +17,15 @@ Route::get('/', function () {
     return view('welcome');
 });
 
+Route::get('/home/search', function () {
+    return view('search');
+});
+
 Route::get('/sample', [\App\Http\Controllers\Sample\IndexController::class, 'show']);
 Route::get('/sample/{id}', [\App\Http\Controllers\Sample\IndexController::class, 'showId']);
 
 //home
 Route::get('/home', \App\Http\Controllers\Home\IndexController::class);
 
-Route::get('/home/search', \App\Http\Controllers\Home\Search\IndexController::class)
-->name('search.index');
-Route::get('/home', \App\Http\Controllers\Home\IndexController::class);
+// Route::get('/home/search', \App\Http\Controllers\Home\Search\IndexController::class)
+// ->name('search.index');
